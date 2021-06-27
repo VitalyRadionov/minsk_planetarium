c82a093ad92f790210a718600a8966a38efe7880
VitalyRadionov <Radiononvv89@gmail.com>
2021-06-27 14:08:18 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.app').vide({
-    // mp4: 'video/video.mp4',
-    // webm: 'video/video.ogv',
-    // ogv: 'video/video.webm',
-    // mov: 'video/video.mov',
+    mp4: 'video/video.mp4',
+    webm: 'video/video.ogv',
+    ogv: 'video/video.webm',
+    mov: 'video/video.mov',
     poster: 'img/cover-img.jpg'
   });
 
