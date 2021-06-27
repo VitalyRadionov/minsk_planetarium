e4df7b4e0385565361a43716a774f71928b826d6
VitalyRadionov <Radiononvv89@gmail.com>
2021-06-27 13:36:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.app').vide({
+    // mp4: 'video/video.mp4',
+    // webm: 'video/video.ogv',
+    // ogv: 'video/video.webm',
+    // mov: 'video/video.mov',
+    poster: 'img/cover-img.jpg'
+  });
+
+  $(".nav-link").click( function () { 
+    $(".nav-link").removeClass('nav-link-color');
+    $(this).addClass('nav-link-color');
+    });
+});
